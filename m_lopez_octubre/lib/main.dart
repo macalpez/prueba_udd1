047e502ff3f5b0e09bc3ab6cea2207ff5dcd6c4b
macalpez <maclopeza@udd.cl>
2024-10-04 16:38:28 -0300

    04/10-16:30

--- m_lopez_octubre/lib/main.dart
+++ m_lopez_octubre/lib/main.dart
@@ -1,125 +1,199 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        appBar: AppBar(
+          leading: const Icon(Icons.arrow_back), 
+          title: const Text('*FRUITOWN*'), 
+          actions: const [
+            Icon(Icons.more_vert), // Ícono de more_vert
+          ],
+        ),
+        body: ListView(
+          children: [
+            Container(
+              height: 100, // Altura de 570 píxeles
+              width: 200, // Ocupa todo el ancho disponible
+              color: Colors.grey, // Color de fondo gris
+              child: const Center(child: Text('Foto portada')), // Texto dentro del primer Container
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const SizedBox(height: 12.0),
+             // Espacio de 12 píxeles entre los containers
+            Container(
+              margin: EdgeInsets.fromLTRB(155, 10, 155, 10),
+              height: 50, // Altura de 148 píxeles
+              width: 50, // Ocupa todo el ancho disponible
+              color: Colors.grey, // Color de fondo azul
+              child: const Center(child: Text('Foto Perfil')), // Texto dentro del segundo Container
             ),
+            const SizedBox(height: 5.0), // Espacio de 12 píxeles entre los containers
+            Container(
+              margin: EdgeInsets.fromLTRB(100, 0, 100, 10),
+              height: 50.0, // Altura de 148 píxeles
+              width: 100, // Ocupa todo el ancho disponible
+              color: Colors.grey, // Color de fondo azul
+              child: const Center(child: Text('Título')), // Texto dentro del tercer Container
+            ),
+            const SizedBox(height: 2.0), // Espacio de 12 píxeles entre los containers
+            Container(
+              margin: EdgeInsets.fromLTRB(140, 0, 140, 10),
+              height: 20.0, // Altura de 148 píxeles
+              width: 100, // Ocupa todo el ancho disponible
+              color: Colors.red, // Color de fondo azul
+              child: const Center(child: Text('Botón')), // Texto dentro del cuarto Container
+            ),
+            const SizedBox(height: 2.0), // Espacio de 12 píxeles entre los containers
+
+            Container(
+              margin: EdgeInsets.fromLTRB(20, 0, 20, 10),
+              height: 60, // Altura de 570 píxeles
+              width: 200, // Ocupa todo el ancho disponible
+              color: Colors.grey, // Color de fondo gris
+              child: const Center(child: Text('Descripción usuario')), // Texto dentro del primer Container
+            ),
+            const SizedBox(height: 1.0),
+             // Espacio de 12 píxeles entre los containers
+            
+            Container(
+              height: 30.0, // Altura de 148 píxeles
+              color: Colors.transparent, // Color de fondo transparente para la separación
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      color: Colors.red, // Color de fondo naranja
+                    ),
+                  ),
+                  const SizedBox(width: 2.0), // Espacio de 12 píxeles entre los containers
+                  Expanded(
+                    child: Container(
+                      color: Colors.red, // Color de fondo naranja
+                    ),
+                  ),
+                  const SizedBox(width: 2.0), // Espacio de 12 píxeles entre los containers
+                  Expanded(
+                    child: Container(
+                      color: Colors.red, // Color de fondo naranja
+                    ),
+                  ),
+                ],
+              ),
+            ),
+                const SizedBox(height: 12.0), // Espacio de 12 píxeles entre los containers
+            Container(
+              margin: EdgeInsets.fromLTRB(10, 0, 200, 10),
+              height: 40, // Altura de 570 píxeles
+              width: 60, // Ocupa todo el ancho disponible
+              color: Colors.grey, // Color de fondo gris
+              child: const Center(child: Text('Subtítulo')), // Texto dentro del primer Container
+            ),
+            const SizedBox(height: 5.0),
+
+            Container(
+              height: 120.0, // Altura de 148 píxeles
+              color: Colors.transparent, // Color de fondo transparente para la separación
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      color: Colors.grey, // Color de fondo naranja
+                    ),
+                  ),
+                  const SizedBox(width: 3.0), // Espacio de 12 píxeles entre los containers
+                  Expanded(
+                    child: Container(
+                      color: Colors.grey, // Color de fondo naranja
+                    ),
+                  ),
+                  
+                ],
+              ),
+            ),
+                  const SizedBox(height: 5.0), // Espacio de 12 píxeles entre los containers
+
+            Container(
+              height: 120.0, // Altura de 148 píxeles
+              color: Colors.transparent, // Color de fondo transparente para la separación
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      color: Colors.grey, // Color de fondo naranja
+                    ),
+                  ),
+                  const SizedBox(width: 3.0), // Espacio de 12 píxeles entre los containers
+                  Expanded(
+                    child: Container(
+                      color: Colors.grey, // Color de fondo naranja
+                    ),
+                  ),
+                  
+                ],
+              ),
+            ),
+            const SizedBox(height: 5.0),
+
+            Container(
+              height: 120.0, // Altura de 148 píxeles
+              color: Colors.transparent, // Color de fondo transparente para la separación
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      color: Colors.grey, // Color de fondo naranja
+                    ),
+                  ),
+                  const SizedBox(width: 3.0), // Espacio de 12 píxeles entre los containers
+                  Expanded(
+                    child: Container(
+                      color: Colors.grey, // Color de fondo naranja
+                    ),
+                  ),
+                  
+                ],
+              ),
+            ),
+            const SizedBox(height: 5.0),
+
+            Container(
+              height: 120.0, // Altura de 148 píxeles
+              color: Colors.transparent, // Color de fondo transparente para la separación
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      color: Colors.grey, // Color de fondo naranja
+                    ),
+                  ),
+                  const SizedBox(width: 3.0), // Espacio de 12 píxeles entre los containers
+                  Expanded(
+                    child: Container(
+                      color: Colors.grey, // Color de fondo naranja
+                    ),
+                  ),
+                  
+                ],
+              ),
+            ),
+
+            
+
+            
+            
+
+            
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
